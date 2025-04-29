828f5413115b95d90cf6f69272366bd7dbb6fa9b
future_potato <phillip@futurepotato.net>
2025-04-29 08:23:17 -0800

    split to import

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from stats import get_word_count
+
 def get_book_text(path):
     # Open the file using a with block
     with open(path) as f:
         # Read the contents and return them
         return f.read()
-def get_word_count(text):
-    words = text.split()
-    return len(words)
 def main():
     path = "./books/frankenstein.txt"
     text = get_book_text(path)
